5024da27dfc3a4689ec72cfe92569ee73f35cc2f
Ratan Kumar <ratan.kumar@witty.com>
2023-01-13 18:36:43 +0530

    6th commit

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import CartIcon from '../Cart/CartIcon'
 import styles from './HeaderCardButton.module.css';
+import { useContext } from 'react'
+import CartContext from '../store/cart-context'
 
 const HeaderCardButton = (props) => {
+  const cartCtx = useContext(CartContext)
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
   return (
     <button className={styles.button} onClick={props.onPressButton}>
-      <span className={styles.icon}><CartIcon/></span>
+      <span className={styles.icon}><CartIcon /></span>
       <span>Your cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,28 @@
-import Input from '../../UI/Input';
+
+import { useRef, useState } from 'react';import Input from '../../UI/Input';
 import styles from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
     <form className={styles.form}>
       <Input
